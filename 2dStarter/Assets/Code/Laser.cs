e7b4a69d00e1ee8d2760f103ae09df62188f76a8
Michael <mrenn2000@gmail.com>
2019-04-29 16:33:18 +0200

    Merged Code
    -Merged Code from MirrorPlacing into Laser.cs;
    -Added Laser-Reflection-Calculation on MouseUp();
    -Added Tag for non reflective GameObjects;

--- 2dStarter/Assets/Code/Laser.cs
+++ 2dStarter/Assets/Code/Laser.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,12 +9,22 @@ public class Laser : MonoBehaviour
 
     private int maxReflectionCount = 10;
     private float maxDistance = 1000000;
-
     private List<Vector3> pointz = new List<Vector3>();
+    private bool hasChanges;
+
+
+    // Davids Klasse;
+
+    private int i = 1;
+    private GameObject box;
+    private Grid[] grid;
+    private Vector3 mouse_pos, tile_pos;
+
+    // David 
 
 
     //private List<Vector2> asds = new List<Vector2>();
- 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,30 +33,65 @@ public class Laser : MonoBehaviour
         lineRenderer.enabled = true; // constantly firering
         lineRenderer.useWorldSpace = true; // origin world points
 
-        // Erzeuge die Linie
-        //drawReflection(transform.position, transform.up, 0, maxReflectionCount);
+        hasChanges = true;
+
+        // Mirrorplacing
+
+        grid = FindObjectsOfType<Grid>();
+
+        // Mirrorplacing
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
 
-        if (Input.GetKey(KeyCode.Space))
+        // Code aus David's Mirrorplacing.cs;
+
+        if (Input.GetMouseButtonDown(0))
         {
-            lineRenderer.enabled = true;
+
+            mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouse_pos[2] = 0; // Setze z == 0;
+
+
+            /*tile_pos = grid[0].WorldToCell(mouse_pos);
+            tile_pos[1] -= 4; 
+            tile_pos[2] = 0;   Klappt alles noch nicht*/
+
+            //Debug.Log(mouse_pos);
+            //Debug.Log(tile_pos);
+
+            box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            box.gameObject.name = "Spiegel " + i;
+
+
+            // Notiz Michael: Warum entfernst du den BoxCollider und addest einen MeshCollider? Wir brauchen doch den BoxColider, weil wir quasi die 3D-Physik verwenden;
+
+            //Destroy(box.GetComponent<BoxCollider>());
+            //box.AddComponent<MeshCollider>();
+
+            box.transform.SetParent(grid[0].transform);
+            box.transform.Rotate(0, 0, 45);
+            box.transform.position = mouse_pos;
+            //box.transform.position = tile_pos;
+            box.transform.localScale = new Vector3(1, 1, 1);
+
+            i++;
         }
-        else
+        if(Input.GetMouseButtonUp(0))
         {
-            lineRenderer.enabled = false;
+            this.setChanges(true);
+            Debug.Log("New Changes!");
         }
 
-        if(Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Backspace))
+        if (hasChanges)
         {
-            Debug.Log("Resetting and recalculating reflection!");
+            Debug.Log("Calculating new Reflection!");
 
             // Reset points;
             pointz = new List<Vector3>();
-            
+
 
             // Calculate new reflection;
 
@@ -65,57 +110,16 @@ public class Laser : MonoBehaviour
                 Debug.Log(pointz[i]);
             }
             Debug.Log("Jobs done!");
+
+            
+            hasChanges = false;
         }
-
-        
-
-        //// [DEBUG-CODE]
-        //if (asds.Count == 0)
-        //{
-        //    Vector2 pos;
-        //    Vector2 dir;
-        //    Debug.Log("Now running test code!");
-
-        //    dir = transform.up;
-        //    pos = transform.position;
-
-        //    Debug.Log(string.Format("Initial Pos {0} Dir {1}", pos, dir));
-
-        //    RaycastHit2D hit = Physics2D.Raycast(pos, dir);
-
-        //    asds.Add(pos);
-        //    pos = hit.point;
-        //    dir = Vector2.Reflect(dir, hit.normal);
-
-        //    Debug.Log(string.Format("[DEBUG-CODE] First Reflection hit at {0} going out with angle {1}", pos, dir));
-
-        //    for (int x = 0; x < maxReflectionCount; x++)
-        //    {
-        //        asds.Add(pos);
-        //        hit = Physics2D.Raycast(pos, dir);
-        //        if (hit)
-        //        {
-        //            pos = hit.point;
-        //            dir = Vector2.Reflect(dir, hit.normal);
-
-        //            Debug.Log(string.Format("[DEBUG-CODE] Reflecting from {0} with angle {1} --- Reflection-Count = {2}", pos, dir, x));
-        //        }
-        //        else
-        //        {
-        //            Debug.Log("[DEBUG-CODE] Kein hit! :(");
-        //        }
-        //    }
-        //}
-
-        //for (int i = 0; i < asds.Count; i++)
-        //{
-        //    Debug.DrawLine(asds[i == 0 ? 0 : i - 1], asds[i == asds.Count - 1 ? i : i + 1]);
-        //}
     }
 
-    private List<Vector3> drawReflection(Vector3 position, Vector3 direction, int bounceCount, int refRemaining, List<Vector3> poss)
+    public List<Vector3> drawReflection(Vector3 position, Vector3 direction, int bounceCount, int refRemaining, List<Vector3> poss)
     {
         Debug.Log(string.Format("----- Reflection #{0} ----- From: {1} - with angle: {2}", bounceCount, position, direction));
+
 
         // Genug reflektiert;
         if (refRemaining == 0)
@@ -131,11 +135,39 @@ public class Laser : MonoBehaviour
         if(Physics.Raycast(position, direction, out RaycastHit hit, maxDistance))
         {
             //Laser trifft irgendwas;
+
+            //Prüfe, welche Objektart getroffen wird;
+            switch (hit.transform.gameObject.tag)
+            {
+                //Ziel erreicht: Beglückwünschen und Rekursion beenden;
+                case "Target":
+                    print("Congrats <3");
+
+                    //Gib grünes Licht;
+                    Renderer targetRenderer = hit.transform.gameObject.GetComponent<Renderer>();
+                    targetRenderer.material.SetColor("_Color", Color.green);
+
+                    //Direction und position aktualisieren auf Treffpunkt;
+                    position = hit.point;
+                    poss.Add(position);
+                    return poss;
+                //Nicht-reflektierender Widerstand;
+                case "NonReflective":
+                    //Direction und position aktualisieren auf Treffpunkt;
+                    position = hit.point;
+                    poss.Add(position);
+                    return poss;
+                default:
+                    print("Hit something else...");
+                    break;
+            }
+
+            //Direction und position aktualisieren auf Treffpunkt;
             direction = Vector3.Reflect(direction, hit.normal);
             position = hit.point;
         }
         else
-        {
+        {   
             //Laser geht ins nix;
             position += direction * maxDistance;
             poss.Add(position);
@@ -147,5 +179,10 @@ public class Laser : MonoBehaviour
         // Recursive call;
         //refRemaining = refRemaining - 1;
         return drawReflection(position, direction, bounceCount+1, refRemaining-1, poss);
+    }
+
+    public void setChanges(bool flag)
+    {
+        this.hasChanges = flag;
     }
 }
