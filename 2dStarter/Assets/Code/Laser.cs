e4afa3056f724fa0a42e77ad00cff6265ae65e4b
Michael <mrenn2000@gmail.com>
2019-04-30 09:12:35 +0200

    Implemented splitting laz0rs
    -Added LaserTrace class;
    -Improved LineRendering;
    -Adjusted drawReflection()-Method;

--- 2dStarter/Assets/Code/Laser.cs
+++ 2dStarter/Assets/Code/Laser.cs
@@ -1,16 +1,37 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+
+
+    public class LaserTrace
+    {
+        public List<Vector3> tracePoints;
+        public List<LaserTrace> nodes;
+
+        public LaserTrace()
+        {
+            tracePoints = new List<Vector3>();
+            nodes = new List<LaserTrace>();
+
+            traceCount++;
+        }
+    }
+
+
     private LineRenderer lineRenderer;
     public Transform LaserHit;
 
     private int maxReflectionCount = 10;
+    private static int traceCount = 0;
+    private int renderCount = 1;
+
     private float maxDistance = 1000000;
-    private List<Vector3> pointz = new List<Vector3>();
+    private LaserTrace pointz = new LaserTrace();
     private bool hasChanges;
+    List<LineRenderer> rendererZ = new List<LineRenderer>();
 
 
     // Davids Klasse;
@@ -79,7 +100,7 @@ public class Laser : MonoBehaviour
 
             i++;
         }
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             this.setChanges(true);
             Debug.Log("New Changes!");
@@ -89,40 +110,93 @@ public class Laser : MonoBehaviour
         {
             Debug.Log("Calculating new Reflection!");
 
+            traceCount = 0;
+
             // Reset points;
-            pointz = new List<Vector3>();
+            pointz = new LaserTrace();
 
 
             // Calculate new reflection;
 
-            pointz.Add(transform.position);
+            pointz.tracePoints.Add(transform.position);
             Debug.Log("Now Reflecting!");
+
+            // Reset "Branch-Renderers"
+            if (rendererZ.Count > 0)
+            {
+                foreach (var ren in rendererZ)
+                {
+                    Destroy(ren);
+                }
+                rendererZ = new List<LineRenderer>();
+            }
+
 
             pointz = drawReflection(transform.position, transform.up, 0, maxReflectionCount, pointz);
 
-            lineRenderer.positionCount = pointz.Count;
-
-            Debug.Log(string.Format("Got {0} reflection points", pointz.Count));
-
-            for (int i = 0; i < pointz.Count; i++)
+            // Draw the in
+            lineRenderer.positionCount = pointz.tracePoints.Count;
+            for (int i = 0; i < pointz.tracePoints.Count; i++)
             {
-                lineRenderer.SetPosition(i, pointz[i]);
-                Debug.Log(pointz[i]);
+                lineRenderer.SetPosition(i, pointz.tracePoints[i]);
             }
+
+            if (rendererZ.Count == 0)
+            {
+                LineRenderer newL = (new GameObject("line" + renderCount++)).AddComponent<LineRenderer>();
+                newL.startWidth = 0.1f;
+                newL.endWidth = 0.1f;
+                newL.enabled = true;
+                rendererZ.Add(newL);
+
+                LineRenderer newR = (new GameObject("line" + renderCount++)).AddComponent<LineRenderer>();
+                newR.startWidth = 0.1f;
+                newR.endWidth = 0.1f;
+                newR.enabled = true;
+                rendererZ.Add(newR);
+            }
+
+            for (int y = 0; y < pointz.nodes.Count; y++)
+            {
+                //LineRenderer newL = (new GameObject("line" + renderCount++)).AddComponent<LineRenderer>();
+
+                Debug.Log("Rendering " + pointz.nodes[y].tracePoints.Count + " #Pointz for line" + y);
+
+                LineRenderer rend = rendererZ[y];
+
+                int n = pointz.nodes[y].tracePoints.Count;
+                rend.positionCount = n;
+                for (int j = 0; j < n; j++)
+                {
+                    rend.SetPosition(j, pointz.nodes[y].tracePoints[j]);
+                    Debug.DrawLine(pointz.nodes[y].tracePoints[j], pointz.nodes[y].tracePoints[j] * 1.2f);
+                }
+            }
+
+            // Old;
+            //lineRenderer.positionCount = pointz.Count;
+            //for (int i = 0; i < pointz.Count; i++)
+            //{
+            //    lineRenderer.SetPosition(i, pointz[i]);
+            //    Debug.Log(pointz[i]);
+            //}
+            //Debug.Log(string.Format("Got {0} reflection points", pointz.Count));
+
+
             Debug.Log("Jobs done!");
 
-            
+
             hasChanges = false;
         }
     }
 
-    public List<Vector3> drawReflection(Vector3 position, Vector3 direction, int bounceCount, int refRemaining, List<Vector3> poss)
+    public LaserTrace drawReflection(Vector3 position, Vector3 direction, int bounceCount, int refRemaining, LaserTrace poss)
     {
         Debug.Log(string.Format("----- Reflection #{0} ----- From: {1} - with angle: {2}", bounceCount, position, direction));
 
 
         // Genug reflektiert;
-        if (refRemaining == 0)
+        if (refRemaining == 0 || traceCount >= 10)
         {
 
             Debug.Log("schüss");
@@ -132,7 +206,7 @@ public class Laser : MonoBehaviour
         // Weiter reflektieren;
 
 
-        if(Physics.Raycast(position, direction, out RaycastHit hit, maxDistance))
+        if (Physics.Raycast(position, direction, out RaycastHit hit, maxDistance))
         {
             //Laser trifft irgendwas;
 
@@ -149,14 +223,69 @@ public class Laser : MonoBehaviour
 
                     //Direction und position aktualisieren auf Treffpunkt;
                     position = hit.point;
-                    poss.Add(position);
+                    poss.tracePoints.Add(position);
                     return poss;
                 //Nicht-reflektierender Widerstand;
                 case "NonReflective":
                     //Direction und position aktualisieren auf Treffpunkt;
                     position = hit.point;
-                    poss.Add(position);
+                    poss.tracePoints.Add(position);
                     return poss;
+                case "PrismaCube":
+                    Debug.Log(string.Format("Hit at: {0}", GetHitFace(hit)));
+                    Debug.Log("Center: " + hit.transform.position);
+
+
+
+                    // Noch benötigt?
+                    //var list1 = drawReflection(position, links, bounceCount + 1, refRemaining - 1, poss);
+                    //return drawReflection(position, direction, bounceCount + 1, refRemaining - 1, list1);
+
+                    // Rechts
+
+                    var rechts = new Vector3(1, 0, 0);
+                    var links = new Vector3(-1, 0, 0);
+
+                    position = hit.transform.position;
+
+
+
+                    // EXPERIMENTAL
+
+                    if (true)
+                    {
+                        var tmp = position;
+                        tmp.x -= 0.5f;
+
+                        LaserTrace left = new LaserTrace();
+                        left.tracePoints.Add(tmp);
+                        left = drawReflection(tmp, links, 0, maxReflectionCount, left);
+                        poss.nodes.Add(left);
+                        Debug.Log("Created Lefthanded reflection");
+                        Debug.Log("Found " + left.tracePoints.Count + " reflection points");
+
+
+                        tmp = position;
+                        tmp.x += 0.5f;
+
+                        LaserTrace right = new LaserTrace();
+                        right.tracePoints.Add(tmp);
+                        right = drawReflection(tmp, rechts, 0, maxReflectionCount, right);
+                        poss.nodes.Add(right);
+                        Debug.Log("Created Righthanded reflection");
+                        Debug.Log("Found " + right.tracePoints.Count + " reflection points");
+                    }
+
+                    // Finish the current line
+                    poss.tracePoints.Add(position);
+                    return poss;
+
+                // EXPERIMENTAL
+
+                //Direction und position aktualisieren auf Treffpunkt;
+
+                //return poss;
+
                 default:
                     print("Hit something else...");
                     break;
@@ -167,22 +296,58 @@ public class Laser : MonoBehaviour
             position = hit.point;
         }
         else
-        {   
+        {
             //Laser geht ins nix;
             position += direction * maxDistance;
-            poss.Add(position);
+            poss.tracePoints.Add(position);
             return poss;
         }
 
-        poss.Add(position);
+        poss.tracePoints.Add(position);
 
         // Recursive call;
         //refRemaining = refRemaining - 1;
-        return drawReflection(position, direction, bounceCount+1, refRemaining-1, poss);
+        return drawReflection(position, direction, bounceCount + 1, refRemaining - 1, poss);
     }
 
     public void setChanges(bool flag)
     {
         this.hasChanges = flag;
+    }
+
+    private enum CubeFace
+    {
+        None,
+        Oben, // Up
+        Down,
+        Rechts, // East
+        Links, // West
+        North,
+        South
+    }
+
+    private CubeFace GetHitFace(RaycastHit hit)
+    {
+        Vector3 incomingVec = hit.normal - Vector3.up;
+
+        if (incomingVec == new Vector3(0, -1, -1))
+            return CubeFace.South;
+
+        if (incomingVec == new Vector3(0, -1, 1))
+            return CubeFace.North;
+
+        if (incomingVec == new Vector3(0, 0, 0))
+            return CubeFace.Oben;
+
+        if (incomingVec == new Vector3(1, 1, 1))
+            return CubeFace.Down;
+
+        if (incomingVec == new Vector3(-1, -1, 0))
+            return CubeFace.Links;
+
+        if (incomingVec == new Vector3(1, -1, 0))
+            return CubeFace.Rechts;
+
+        return CubeFace.None;
     }
 }
